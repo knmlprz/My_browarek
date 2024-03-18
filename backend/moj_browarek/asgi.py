62c34ebdc7cfc109dcc01c27080acdc94fc8f164
DamianKobylinski <damiankob12@gmail.com>
2024-03-18 22:28:33 +0100

    backend root

---
+++ backend/moj_browarek/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for moj_browarek project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moj_browarek.settings')
+
+application = get_asgi_application()
---
+++ backend/moj_browarek/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for moj_browarek project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'moj_browarek.settings')
+
+application = get_wsgi_application()
