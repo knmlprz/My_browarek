ee82ca8b9dc208ccaa26d5904b098570fa7a1e8a
fwalkowicz <walkowiczf@gmail.com>
2024-04-13 17:34:58 +0200

    make migrations and register userProfile

--- backend/user/models.py
+++ backend/user/models.py
@@ -1,11 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core import validators, exceptions
-import datetime
+from django.utils import timezone
 
 
 def validate_birth_date(birth_date):
-    today = datetime.date.today()
+    today = timezone.now
     age = (today - birth_date).days / 365
     if age < 18:
         raise exceptions.ValidationError("Użytkownik musi być pełnoletni")
@@ -22,7 +22,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     weight = models.FloatField(blank=True, null=True, validators=[validators.MinValueValidator(0), validators.MaxValueValidator(610)])
     height = models.FloatField(blank=True, null=True, validators=[validators.MinValueValidator(0), validators.MaxValueValidator(251)])
-    birthDate = models.DateField(default=datetime.date.today(), blank=False, validators=[validate_birth_date])
+    birthDate = models.DateField(default=timezone.now, blank=False, validators=[validate_birth_date])
 
     def __str__(self):
         return {UserProfile.user}
