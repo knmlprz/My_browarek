fa8c4201a1e3ea581c4fc1e46d3038682eba2c9e
fwalkowicz <walkowiczf@gmail.com>
2024-04-14 19:35:45 +0200

    register new form for userProfile

--- backend/user/admin.py
+++ backend/user/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import UserProfile
+from django.contrib import admin
+from .forms import UserProfileForm
+
 
 # Register your models here.
-admin.site.register(UserProfile)
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    form = UserProfileForm
